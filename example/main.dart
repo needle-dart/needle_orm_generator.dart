d361bce43f57a4342164d51a74d524e47fc6a0fa
outersky <outersky@gmail.com>
2022-02-26 10:35:11 +0800

    fix: small

--- example/main.dart
+++ example/main.dart
@@ -5,62 +5,9 @@ import 'package:scope/scope.dart';
 
 import 'src/domain.dart';
 
-class MockDataSource extends DataSource {
-  MockDataSource() : super(DatabaseType.PostgreSQL, '10.0');
-  @override
-  Future<List<List>> execute(
-      String tableName, String sql, Map<String, dynamic> substitutionValues,
-      [List<String> returningFields = const []]) async {
-    print('[sql] $sql [bindings: $substitutionValues]');
-    return List<List>.empty();
-    // throw UnimplementedError();
-  }
-
-  @override
-  Future<T> transaction<T>(FutureOr<T> Function(DataSource p1) f) {
-    throw UnimplementedError();
-  }
-}
-
 void main() {
   DataSource ds = MockDataSource();
   (Scope()..value<DataSource>(scopeKeyDefaultDs, ds)).run(test);
-}
-
-void main2() {
-  final v = Vector(2, 3);
-  final w = Vector(2, 2);
-
-  assert(v + w == Vector(4, 5));
-  assert(v - w == Vector(0, 1));
-  print(v < w == Vector(6, 7));
-  print(v < w);
-}
-
-class Vector {
-  final int x, y;
-
-  Vector(this.x, this.y);
-
-  Vector operator +(Vector v) => Vector(x + v.x, y + v.y);
-  Vector operator -(Vector v) => Vector(x - v.x, y - v.y);
-
-  Vector operator <(Vector v) => Vector(x + 2 * v.x, y + 2 * v.y);
-
-  // Operator == and hashCode not shown.
-  // ···
-  @override
-  int get hashCode => Object.hash(x, y);
-
-  @override
-  bool operator ==(dynamic other) {
-    return other is Vector && other.x == x && other.y == y;
-  }
-
-  @override
-  String toString() {
-    return "Vector[x=$x,y=$y]";
-  }
 }
 
 void test() {
@@ -87,4 +34,31 @@ void test() {
     ..author = user
     ..title = 'Dart'
     ..insert();
+
+  Book.Query.findAll();
+
+/* 
+  Book.Query
+        ..name ~ 'dart'
+        ..age > 10
+        ..(author..name = 'john'
+                 ..age > 30)
+ */
+}
+
+class MockDataSource extends DataSource {
+  MockDataSource() : super(DatabaseType.PostgreSQL, '10.0');
+  @override
+  Future<List<List>> execute(
+      String tableName, String sql, Map<String, dynamic> substitutionValues,
+      [List<String> returningFields = const []]) async {
+    print('[sql] $sql [bindings: $substitutionValues]');
+    return List<List>.empty();
+    // throw UnimplementedError();
+  }
+
+  @override
+  Future<T> transaction<T>(FutureOr<T> Function(DataSource p1) f) {
+    throw UnimplementedError();
+  }
 }
